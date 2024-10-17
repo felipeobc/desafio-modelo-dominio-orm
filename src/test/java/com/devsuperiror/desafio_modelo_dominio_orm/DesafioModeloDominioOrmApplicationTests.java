deed1d25c5fa7bcf20f69bedd9a47120887a36b9
Felipe Camargo T786510 <felipe.camargo@f1rst.com.br>
2024-10-16 22:38:07 -0300

    update code

---
+++ src/test/java/com/devsuperiror/desafio_modelo_dominio_orm/DesafioModeloDominioOrmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperiror.desafio_modelo_dominio_orm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioModeloDominioOrmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
